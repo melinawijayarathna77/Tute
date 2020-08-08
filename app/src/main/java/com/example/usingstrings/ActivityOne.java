0ac63e695dba5a1172111eab029df1a5dc0412ca
melinawijayarathna77 <melinawijayarathna76@gmail.com>
2020-08-08 23:43:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/usingstrings/ActivityOne.java
@@ -0,0 +1,64 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class ActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("Lifecycle","OnCreate() invoked");
+
+        TextView txtView2;
+        txtView2 = findViewById(R.id.textView2);
+        txtView2.setText(R.string.Msg2);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i("Lifecycle","OnStart() invoked");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i("Lifecycle","OnRestart() invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i("Lifecycle","OnResume() invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i("Lifecycle","OnPause() invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i("Lifecycle","OnStop() invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i("Lifecycle","OnDestroy() invoked");
+    }
+}
